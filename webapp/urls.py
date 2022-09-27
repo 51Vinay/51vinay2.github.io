d5b3e32800b1ef3f9b3370bc5651714c97542d0d
Vinay <vinaysharma5335@gmail.com>
2022-09-27 11:58:28 +0530

    allld

--- webapp/urls.py
+++ webapp/urls.py
@@ -19,5 +19,10 @@ from webpage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.homePage)
+    path('',views.homePage),
+    path('emp', views.emp),  
+    path('show/',views.show),  
+    path('edit/<int:id>', views.edit),  
+    path('update/<int:id>', views.update),  
+    path('delete/<int:id>', views.destroy),  
 ]
